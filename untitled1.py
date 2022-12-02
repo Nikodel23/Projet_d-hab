ca04bc010edb10c1d88fd6ff9e9bce7153ac4ad2
Nikodel23 <113675244+Nikodel23@users.noreply.github.com>
2022-12-02 12:06:11 +0100

    Create untitled1.py
    hagrah

---
+++ untitled1.py
@@ -0,0 +1,14 @@
+text_intro = ("\n\
+\n Bienvenue dans le dab de MOULA BANQUE, \
+\n\
+\n Vous pourrez ici consulter votre compte banquaire et effectué divers fonctions ")
+
+action_for_conseiller = ("\n\
+nous vennons de vérifier votre identifiant, vous êtes bien conseiller \
+\nVoici les actions que vous pouvez effectuer selon le numéro que vous entrez :\
+\n\
+\n 1 : Consulter votre compte \
+\n 2 : Effectuer un retrait sur votre compte \
+\n 3 : Effectuer un dépot sur votre compte \
+\n 4 : Consulter votre historique \
+\n 5 : Consulter le compte bancaire de vos clients") 
