af7c67bb1df120e6d640582e929db84a157bd0e3
NoahBrz <113674977+NoahBrz@users.noreply.github.com>
2023-01-06 11:14:56 +0100

    code 2

---
+++ data_3.py
@@ -0,0 +1,40 @@
+text_intro = ("\n\
+\n Bienvenue dans le dab de MOULA BANQUE, \
+\n\
+\n Vous pourrez ici consulter votre compte banquaire et effectué divers fonctions ")
+
+text_for_action_for_clients = ("\n\
+nous vennons de vérifier votre identifiant, vous êtes bien clients \
+\nVoici les actions que vous pouvez zffzctué selon le numéro que vous entrez :\
+\n\
+\n 1 : Consulter votre compte \
+\n 2 : Effectuer un retrait sur votre compte \
+\n 3 : Effectuer un dépot sur votre compte \
+\n 4 : Consulter votre historique")
+
+text_for_action_for_advisor = ("\n\
+nous vennons de vérifier votre identifiant, vous êtes bien conseiller \
+\nVoici les actions que vous pouvez zffzctué selon le numéro que vous entrez :\
+\n\
+\n 0 : Consulter la liste des clients \
+\n 1 : Consulter votre compte \
+\n 2 : Effectuer un retrait sur votre compte \
+\n 3 : Effectuer un dépot sur votre compte \
+\n 4 : Consulter votre historique")
+
+text_action_withdrawal = ("Vous avez choissie de faire un retrait \
+\n\
+\nQuel est le montant de votre retrait ? ")
+
+text_action_deposit = ("Vous avez choissie de faire un dépot \
+\n\
+\nQuel est le montant de votre dépot ? ")
+
+text_action_history = ("Vous avez choisie de consulter votre historique \
+\n\
+\Voici votre historique : ")
+
+text_action_account = (("Vous venez de consulter votre compte, \
+\n\
+\n Votre nom est : ") + ("") + ("\n\ \n Votre mot de passe est :") + ("") + \
+("Il vous reste actuellement sur votre compte ") + ("") + ("€."))
---
+++ menu_projet3.py
@@ -0,0 +1,44 @@
+from data_3 import (text_intro,text_for_action_for_clients, \
+                    text_for_action_for_advisor,text_action_withdrawal,\
+                    text_action_deposit, text_action_history, \
+                    text_action_account)
+
+#identifier la personne
+intro = print(text_intro)
+statut = "client"
+if statut == "client":
+    def menu_for_clients():
+        print (text_for_action_for_clients)
+        action = int(input("Quel est votre action ?"))
+        if action == 1:
+            print(text_action_account)
+        if action == 2:
+            print (text_action_withdrawal)
+            amount = int(input("montant :"))
+            return amount
+        if action == 3:
+            print (text_action_deposit)
+            deposit = int(input("dépot : "))
+            return deposit
+        if action == 4:
+            print(text_action_history)
+
+print (menu_for_clients())
+  
+if statut == "conseiller ":
+    def menu_for_advisor ():
+        print (text_for_action_for_advisor)
+        action = int(input("Quel est votre action ?"))
+        if action == 0:
+            pass
+        if action == 1:
+            pass
+        if action == 2:
+            pass
+        if action == 3:
+            pass
+        if action == 4:
+            pass
+        
+        
+    
