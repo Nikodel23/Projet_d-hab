4cea733e56b8cb6323ddf2fae33c37101f4c70a0
LeRobbii <113675407+LeRobbii@users.noreply.github.com>
2023-01-09 20:18:44 +0100

    V2 identification part

--- aurélien_et_sa_production.py
+++ aurélien_et_sa_production.py
@@ -5,20 +5,20 @@ Created on Mon Jan  9 19:42:59 2023
 @author: Aurélien
 """
 
-identification = "Pouvez vous nous indiquez votre nom et prénom ?\n"
+identification = "\n\Pouvez vous nous indiquez votre nom et prénom ? "
 
-identification = input ("Pouvez vous nous indiquez votre nom et prénom ?\n")
+identification = input ("\n\Pouvez vous nous indiquez votre nom et prénom ? ")
 if identification == #nom d'un client
-    mot_de_passe = input ("Veuillez entrer votre mot de passe:/n")
+    mot_de_passe = input ("\n\Veuillez entrer votre mot de passe: ")
     if mot_de_passe == #mdp du client en question
     statut = "client "
     else:
         pass
 elif identification == #nom d'un conseiller
-    mot_de_passe = input ("Veuillez entrer votre mot de passe:/n")
+    mot_de_passe = input ("\n\Veuillez entrer votre mot de passe: ")
     if mot_de_passe == #mdp du conseiller en question
     statut = "conseiller "
     else:
         pass
 else:
-    identification_erronee = input ("Merci de réinsérer votre nom et prénom :/n")
+    identification_erronee = input ("\n\Merci de réinsérer votre nom et prénom : ")
