be13a57b9eb386fee17ae3d2f76a6882fc966ea3
LeRobbii <113675407+LeRobbii@users.noreply.github.com>
2023-01-09 20:15:10 +0100

    V1 identification part

--- aurélien_et_sa_production.py
+++ aurélien_et_sa_production.py
@@ -5,4 +5,20 @@ Created on Mon Jan  9 19:42:59 2023
 @author: Aurélien
 """
 
-test
+identification = "Pouvez vous nous indiquez votre nom et prénom ?\n"
+
+identification = input ("Pouvez vous nous indiquez votre nom et prénom ?\n")
+if identification == #nom d'un client
+    mot_de_passe = input ("Veuillez entrer votre mot de passe:/n")
+    if mot_de_passe == #mdp du client en question
+    statut = "client "
+    else:
+        pass
+elif identification == #nom d'un conseiller
+    mot_de_passe = input ("Veuillez entrer votre mot de passe:/n")
+    if mot_de_passe == #mdp du conseiller en question
+    statut = "conseiller "
+    else:
+        pass
+else:
+    identification_erronee = input ("Merci de réinsérer votre nom et prénom :/n")
