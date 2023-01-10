937ba65db4e8f750fe4ceda7dfaac988c142c1db
Nikodel23 <113675244+Nikodel23@users.noreply.github.com>
2023-01-10 21:03:53 +0100

    empreint , thune, solde
    Je me suis donné à fond

--- untitled1.py
+++ untitled1.py
@@ -18,38 +18,47 @@ Nous vennons de vérifier votre identifiant, vous êtes bien conseiller \
 \n 4 : Consulter votre historique \
 \n 5 : Consulter le compte bancaire de vos clients") 
 
+from menu_projet3.py import menu_for_clients
 
-def text_action_account():
+def text_action_account(identification,mot_de_passe,solde):
     print(identification)
     print(mot_de_passe)
-    print(Moula)
+    print(solde)
     if text_action_account == 0:
         print(menu_for_clients)
         
         
-def text_action_withdrawal():
-    print(Moula)
-    if text_action_withdrawal == 0:
-        print(menu_for_clients)
-    if text_action_withdrawal == 1:
-        i>=0
-        liste=[10,20,50,100,200,500]
-        dico_client = i
-         
+def text_action_withdrawal(solde,somme):
+            print(solde)
+            if text_action_withdrawal == 0:
+                print(menu_for_clients)
+                if text_action_withdrawal == 1:
+                    liste_de_possibilté_de_prélevement=[10,20,50,100,200,500]
+                    somme = liste_de_possibilté_de_prélevement
+                    solde = solde + somme
+                    print(solde)
 
-def text_action_deposit():
-    print(Moula)
-    if text_action_deposit == 0:
-        print(menu_for_clients)
-    if text_action_deposit == 1:
-        i>=0
-        liste=[10,20,50,100,200,500]
-    
-    
-def text_action_history():
-    print(Moula)
-    print(Moula_history)
-        if text_action_history == 0:
-            print(menu_for_clients)
+
+
+def text_action_deposit(solde,somme):
+            print(solde)
+            if text_action_deposit == 0:
+                print(menu_for_clients)
+                if text_action_deposit == 1:
+                    liste_de_possibilté_de_prélevement=[10,20,50,100,200,500]
+                    somme = liste_de_possibilté_de_prélevement
+                    if (somme > solde):
+                        print("Solde insuffisante!")
+                else:
+                    solde = solde-somme
+                print(solde)
+        
+        
+        
+def text_action_history(solde,solde_history):
+            print(solde)
+            print(solde_history)
+            if text_action_history == 0:
+                print(menu_for_clients)
     
      
