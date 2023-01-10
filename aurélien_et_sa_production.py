de9c110c76ca07c53a145ce40d1a0e4d7f3cbd3d
LeRobbii <113675407+LeRobbii@users.noreply.github.com>
2023-01-10 16:16:38 +0100

    V6

--- aurélien_et_sa_production.py
+++ aurélien_et_sa_production.py
@@ -4,20 +4,14 @@ Created on Mon Jan  9 19:42:59 2023
 
 @author: Aurélien
 """
-
 list_name_advice = {"niko","Aurelienne"}
 list_name_advisor = {"noah","Poutine"}
 identification = "\n\Pouvez vous nous indiquez votre nom et prénom ? "
-list_mdp_advice = "123"
-list_mdp_advisor = "234"
+
 
 def mot_de_passe_fct():
     mot_de_passe = input ("\nVeuillez entrer votre mot de passe: ")
     return mot_de_passe
-
-def mot_de_passe_erronnee():
-    print ("\nVotre mot de passe est incorrect.\n\n")
-    return mot_de_passe_fct()
 
 def identification_fct():
     statut = input("Quel est votre statut ? ")
@@ -28,12 +22,7 @@ def identification_fct():
         else:
             statut = "client"
             print (statut)
-            mot_de_passe = input ("\nVeuillez entrer votre mot de passe: ")
-            if mot_de_passe not in list_mdp_advice:
-                mot_de_passe_erronnee()
-            else:
-                print("all good t'es un bg")
-        return (statut)
+            return (statut)
     if statut == "conseiller":
         if identification not in list_name_advisor :
             identification_erronee()
