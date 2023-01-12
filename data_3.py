d723108bd48ab59d83241cdedef328f8d1843118
NoahBrz <113674977+NoahBrz@users.noreply.github.com>
2023-01-12 20:45:30 +0100

    code 7

--- data_3.py
+++ data_3.py
@@ -34,7 +34,3 @@ text_action_history = ("Vous avez choisie de consulter votre historique \
 \n\
 \Voici votre historique : ")
 
-text_action_account = (("Vous venez de consulter votre compte, \
-\n\
-\n Votre nom est : ") + ("") + ("\n\ \n Votre mot de passe est :") + ("") + \
-("Il vous reste actuellement sur votre compte ") + ("") + ("€."))
